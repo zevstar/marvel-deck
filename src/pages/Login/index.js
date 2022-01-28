4ef8a702da76d29f44237edab3008137ed67b1d9
Alana Lipkin <alana.lipkin@gmail.com>
2022-01-28 18:07:05 -0500

    added useNavigate

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 import './styles.css'
 
 const Login = ({ setUser }) => {
     const [username, setUsername] = useState('')
+    const navigate = useNavigate()
 
     const handleChange = e => {
         setUsername(e.target.value)
@@ -11,6 +13,7 @@ const Login = ({ setUser }) => {
     const handleSubmit = e => {
         e.preventDefault()
         setUser(username)
+        navigate('/marvel/list')
     }
 
 
