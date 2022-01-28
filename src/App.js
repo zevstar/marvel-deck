80ca958c04370382415509fb37648202e10dfc48
Alana Lipkin <alana.lipkin@gmail.com>
2022-01-28 12:46:03 -0500

    updated Nav to work with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+// css
 import './App.css';
+
+// pages
 import './pages/Home';
 import './pages/Login';
+//components
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      App.js
+     
+      <Nav />
+      {/* <Login /> */}
     </div>
   );
 }
