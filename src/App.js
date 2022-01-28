d5f3ccca17c6c647c07011a591ae7c287e01ecd1
Alana Lipkin <alana.lipkin@gmail.com>
2022-01-28 16:26:01 -0500

    udpaded Nav login and added Nav css file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom'
 // css
 import './App.css';
 //contexts
-import UserContext from './contexts/UserContext.js'
+import UserContext from './contexts/UserContext.js';
 // pages
 import './pages/Home';
-import './pages/Login';
+import Login from './pages/Login';
 //components
-import Nav from './components/Nav'
+import Nav from './components/Nav';
 
 function App() {
-  // const user = useContext(UserContext)
-  // console.log(user)
-  return (
-    <div className="App">
-     <UserContext.Provider value={'filler'}>
+	// const user = useContext(UserContext)
+	// console.log(user)
+	return (
+		<div className='App'>
+			<UserContext.Provider value={'filler'}>
+				<Nav />
 
-        <Nav />
+				<Routes>
+          <Route path='login' element={<Login />} />
+          {/* <Route path='home' element ={<Home />} /> */}
+        </Routes>
+			</UserContext.Provider>
 
-     </UserContext.Provider>
-
-      
-      {/* <Login /> */}
-    </div>
-  );
+			{/* <Login /> */}
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,39 @@
 // import React from 'react';
+import './styles.css'
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
-}
+	return (
+		<form className='mx-auto border p-2 m-2' id='login-form'>
+			<div className='mb-3'>
+				<label htmlFor='exampleInputUser1' className='form-label'>
+					Username
+				</label>
+				<input
+					type='text'
+					className='form-control'
+					id='exampleInputUser1'
+					aria-describedby='userHelp'
+				/>
+				<div id='userHelp' className='form-text'>
+					We will lie to you.
+				</div>
+			</div>
+			<div className='mb-3'>
+				<label htmlFor='exampleInputPassword1' className='form-label'>
+					Password
+				</label>
+				<input
+					type='password'
+					className='form-control'
+					id='exampleInputPassword1'
+				/>
+			</div>
+
+			<button type='submit' className='btn btn-primary'>
+				Submit
+			</button>
+		</form>
+	);
+};
 
 export default Login;
